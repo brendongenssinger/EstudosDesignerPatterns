60867811829ff593816981aec0dc8170dde63941
Brendon Mascarenhas <brendon.genssinger@gmail.com>
2022-02-07 21:59:10 -0300

    Implementação de um novo Imposto ( ICCC )

---
+++ Alura/Strategy/ICCC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Designer_Pattern.Alura.Strategy
+{
+    public class ICCC : IImpostos
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (orcamento.Valor < 1000)
+                return orcamento.Valor * 0.05;
+            else if (orcamento.Valor >= 1000 && orcamento.Valor <= 3000)
+                return orcamento.Valor * 0.07;
+            else
+                return (orcamento.Valor * 0.08) + 30;
+
+        }
+    }
+}
--- Alura/Strategy/TesteImposto.cs
+++ Alura/Strategy/TesteImposto.cs
@@ -12,12 +12,17 @@ namespace Designer_Pattern.Alura.Strategy
         {
             IImpostos icms = new ICMS();
             IImpostos iss = new ISS();
+            IImpostos ccc = new ICCC();
             Orcamento orcamento = new Orcamento(500.00);
 
             CalculadorDeImpostos calc = new CalculadorDeImpostos();
 
             calc.RealizaCalculo(orcamento,icms);
             calc.RealizaCalculo(orcamento, iss);
+            calc.RealizaCalculo(orcamento, ccc);
+
+            ccc.Calcula(orcamento);
+
         }
     }
 }
