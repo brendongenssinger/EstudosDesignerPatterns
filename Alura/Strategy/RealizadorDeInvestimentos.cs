9a974029af0ccfd35f508c0dde7edd9b63ab3572
Brendon Mascarenhas <brendon.genssinger@gmail.com>
2022-03-24 11:14:22 -0300

    Novas implementações do Strategy.

--- Alura/Strategy/RealizadorDeInvestimentos.cs
+++ Alura/Strategy/RealizadorDeInvestimentos.cs
@@ -8,6 +8,56 @@ namespace Designer_Pattern.Alura.Strategy
 {
     public class RealizadorDeInvestimentos
     {
+        public void CalculoInvestimento(ContaCorrente pContaCorrente, TipoInvestimento tipoInvestimento) 
+        {
+            double retorno = 0.00;
+            int chance = 0;
 
+            switch (tipoInvestimento)
+            {
+                case TipoInvestimento.CONSERVADOR:
+                    retorno = pContaCorrente.Saldo * 0.8;
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno+pContaCorrente.Saldo}");
+                    break;
+                case TipoInvestimento.MODERADO:
+                    
+                    retorno = pContaCorrente.Saldo * 2.5;
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} 2.5%");
+                    retorno = pContaCorrente.Saldo * 0.7;
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} 0.7%");
+
+                    break;
+                case TipoInvestimento.ARROJADO:
+                    chance = new Random().Next(101);
+                    
+                        retorno = pContaCorrente.Saldo * 5;
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} - chance 20%");
+                    retorno = pContaCorrente.Saldo * 3;                                  
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} - chance 30%");
+                    retorno = pContaCorrente.Saldo * 0.6;                                
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} - chance 50%");                    
+                    break;
+                default:
+                    Console.WriteLine($"Saldo {tipoInvestimento.ToString().ToUpper()} R${retorno + pContaCorrente.Saldo} ");
+                    break;
+            }
+        }
+    }
+
+    public class ContaCorrente
+    {
+        public int Saldo { get; private set; }
+        public ContaCorrente(int pSaldo)
+        {
+            Console.WriteLine("Saldo => "+pSaldo);
+            this.Saldo = pSaldo;
+        }
+    }
+
+    public enum TipoInvestimento 
+    {
+        CONSERVADOR,
+        MODERADO,
+        ARROJADO
     }
 }
--- Alura/Strategy/TesteImposto.cs
+++ Alura/Strategy/TesteImposto.cs
@@ -10,6 +10,28 @@ namespace Designer_Pattern.Alura.Strategy
     {
         public static void Main(string[] args) 
         {
+            
+            CalcImpost();
+            CalcInvestimento();
+            Console.ReadKey();
+
+        }
+
+        private static void CalcInvestimento()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Calculo de Investimento");
+            var realizadorDeInvestimento = new RealizadorDeInvestimentos();
+            realizadorDeInvestimento.CalculoInvestimento(new ContaCorrente(500), TipoInvestimento.CONSERVADOR);
+            realizadorDeInvestimento.CalculoInvestimento(new ContaCorrente(500), TipoInvestimento.MODERADO);
+            realizadorDeInvestimento.CalculoInvestimento(new ContaCorrente(500), TipoInvestimento.ARROJADO);
+
+        }
+
+        private static void CalcImpost()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Calculo de imposto");
             IImpostos icms = new ICMS();
             IImpostos iss = new ISS();
             IImpostos ccc = new ICCC();
@@ -17,12 +39,13 @@ namespace Designer_Pattern.Alura.Strategy
 
             CalculadorDeImpostos calc = new CalculadorDeImpostos();
 
-            calc.RealizaCalculo(orcamento,icms);
+            calc.RealizaCalculo(orcamento, icms);
             calc.RealizaCalculo(orcamento, iss);
             calc.RealizaCalculo(orcamento, ccc);
 
-            ccc.Calcula(orcamento);
-
+            ccc.Calcula(orcamento);            
+            Console.WriteLine("Finished Calculo de imposto");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
